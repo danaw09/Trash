ba1488d8952d9cd4bc28f6b2dfc3890126376883
dana <danaw09@gmail.com>
2019-08-10 12:07:08 -0500

    inital commit

---
+++ TrashProject/TrashProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrashProject.MvcApplication" Language="C#" %>
---
+++ TrashProject/TrashProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashProject.Startup))]
+namespace TrashProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
