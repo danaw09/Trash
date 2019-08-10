120089a285a0ef5ae3f96c7094804ebf899c721e
dana <danaw09@gmail.com>
2019-08-10 17:39:03 -0500

    added addrole view to the addrole controller and updated datbase

---
+++ TrashProject/TrashProject/Controllers/RoleController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrashProject.Models;
+
+namespace TrashProject.Controllers
+{
+
+    public class RoleController : Controller
+    {
+        private ApplicationDbContext _context;
+        public RoleController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            IdentityRole Role = new IdentityRole();
+            return View(Role);
+        }
+
+        public ActionResult Index()
+        {
+
+            if (User.Identity.IsAuthenticated)
+            {
+
+
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var Roles = _context.Roles.ToList();
+            return View(Roles);
+
+        }
+
+        private bool isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+}   }
+
+
